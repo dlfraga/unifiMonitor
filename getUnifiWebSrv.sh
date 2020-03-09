bf6eb51740d44144e5d4d67a66a4155b8deb1116
root <root@webserver.smartsupport.com.br>
2020-03-09 11:18:52 -0300

    returned to the env file configuration

--- getUnifiWebSrv.sh
+++ getUnifiWebSrv.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-unifiUrl="$1"
-unifiUsername=$2
-unifiPassword=$3
-apiUrlToQuery="$4"
+unifiUrl=$(grep unifiUrl $DIR/.unifi-env | cut -d '=' -f2)
+unifiUsername=$(grep unifiUsername $DIR/.unifi-env | cut -d '=' -f2)
+unifiPassword=$(grep unifiPassword $DIR/.unifi-env | cut -d '=' -f2)
+authCookiePath=$(grep authCookiePath $DIR/.unifi-env | cut -d '=' -f2)
 
-authCookiePath=cookiejar
-logFile=connections.log
+apiUrlToQuery=$1
+authCookiePath=$DIR/cookiejar
+logFile=$DIR/connectionsUnifi.log
+curlBin=/usr/bin/curl
 isLoggedIn=0
 response=""
 
@@ -40,19 +43,31 @@ function checkLogin() {
 isLoggedIn=0
 }
 
+function validatePaths() {
+local curlBin=$(which curl)
+
+if [ $curlBin == ""  ]; then
+	echo "Cannot find curl. Please set your PATH variable or define curl path manually."
+	echo "[\"errorMsg\",\"Cannot find the curl command\"]"
+	exit 1;
+fi
+}
+
 function login () {
   echo "Trying to login" >> $logFile
-  response=$(curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"$unifiUsername\",\"password\":\"$unifiPassword\",\"remember\":false,\"strict\":true}" -s "$unifiUrl"/api/login -c $authCookiePath)
+  response=$(curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"$unifiUsername\",\"password\":\"$unifiPassword\",\"remember\":false,\"strict\":true}" -s $unifiUrl/api/login -c $authCookiePath)
+  echo "curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"$unifiUsername\",\"password\":\"$unifiPassword\",\"remember\":false,\"strict\":true}" -s $unifiUrl/api/login -c $authCookiePath" >> $logFile
   echo $response >> $logFile
 }
 
 function query() {
 #Query the unifi api
-  response=$(curl -X GET -b $authCookiePath -s "$unifiUrl"/"$1")
+  response=$(curl -X GET -b $authCookiePath -s "$unifiUrl"/"$apiUrlToQuery")
   echo $response >> $logFile
   echo $response
 }
 
+validatePaths
 checkLogin
 
 if [ "$isLoggedIn" == 1 ]; then
@@ -62,8 +77,9 @@ else
   checkLogin
   if [ "$isLoggedIn" == 0 ]; then
     echo "Can't login to the unifi dashboard. Please check the logs and responses" >> $logFile
+    echo "[\"errorMsg\",\"Cannot login to the unifi API\"]"
     exit 1
   fi
 fi
 
-query $1
+query $apiUrlToQuery
